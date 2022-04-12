8b17aa404540c775073d320fe68ea9e95aa5f0a0
alfonmga <alfonmga@icloud.com>
2022-04-12 19:54:47 +0200

    Update

---
+++ 3.longest_substring_without_repeating_characters/main.go
@@ -0,0 +1,33 @@
+package longest_substring_without_repeating_characters
+
+import "strings"
+
+func LengthOfLongestSubstring(s string) int {
+	sChars := strings.Split(s, "")
+
+	startSCharPosIdx := 0
+	longestSubsStrCounter := 0
+	encounterChars := make(map[string]bool)
+	currentStreakCounter := 0
+	for i := 0; i < len(sChars); {
+		c := sChars[i]
+
+		_, found := encounterChars[c]
+		if found {
+			encounterChars = make(map[string]bool) // nuke dict
+			currentStreakCounter = 0               // reset streak counter
+			startSCharPosIdx += 1
+			i = startSCharPosIdx
+			continue
+		}
+
+		encounterChars[c] = true
+		currentStreakCounter += 1
+		if currentStreakCounter > longestSubsStrCounter {
+			longestSubsStrCounter += 1
+		}
+		i++
+	}
+
+	return longestSubsStrCounter
+}
---
+++ 3.longest_substring_without_repeating_characters/main_test.go
@@ -0,0 +1,29 @@
+package longest_substring_without_repeating_characters
+
+import (
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	var tests = []struct {
+		s   string
+		ans int
+	}{
+		{"abcabcbb", 3},
+		{"bbbbb", 1},
+		{"pwwkew", 3},
+		{" ", 1},
+		{"aab", 2},
+		{"dvdf", 3},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			actual := LengthOfLongestSubstring(tt.s)
+			if actual != tt.ans {
+				t.Errorf("got %v, want %d", actual, tt.ans)
+			}
+		})
+	}
+
+}
