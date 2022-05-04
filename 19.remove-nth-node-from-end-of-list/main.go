8ece7232593610e40787e94cd5e1b988dfde94ba
alfonmga <alfonmga@icloud.com>
2022-05-04 23:20:38 +0200

    Update

--- 19.remove-nth-node-from-end-of-list/main.go
+++ 19.remove-nth-node-from-end-of-list/main.go
@@ -6,5 +6,22 @@ type ListNode struct {
 }
 
 func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
-	return &ListNode{Val: 0, Next: nil}
+	var linkedList []*ListNode
+	lastN := head
+	for {
+		linkedList = append(linkedList, lastN)
+		if lastN.Next != nil {
+			lastN = lastN.Next
+		} else {
+			break
+		}
+	}
+
+	prevTargetN := linkedList[len(linkedList)-n-1]
+	nextTargetN := linkedList[len(linkedList)-n+1]
+	prevTargetN.Next = nextTargetN
+
+	linkedList = append(linkedList[:len(linkedList)-n], linkedList[len(linkedList)-n+1:]...)
+
+	return linkedList[0]
 }
