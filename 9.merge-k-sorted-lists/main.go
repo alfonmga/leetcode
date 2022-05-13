92550fc2eff01e969ad502929efc0a44212a39f2
alfonmga <alfonmga@icloud.com>
2022-05-13 18:09:25 +0200

    Update

--- 9.merge-k-sorted-lists/main.go
+++ 9.merge-k-sorted-lists/main.go
@@ -1,10 +1,32 @@
 package merge_k_sorted_lists
 
+import "sort"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func MergeKLists(lists []*ListNode) *ListNode {
-	return &ListNode{Val: 0, Next: nil}
+	var valuesFromLists []int
+	for i := 0; i < len(lists); i++ {
+		n := lists[i]
+		for n != nil {
+			valuesFromLists = append(valuesFromLists, n.Val)
+			n = n.Next
+		}
+	}
+	sort.Ints(valuesFromLists)
+
+	var linkedList = make([]ListNode, len(valuesFromLists))
+	for i := 0; i < len(valuesFromLists); i++ {
+		var nextN *ListNode
+		if i != len(valuesFromLists)-1 {
+			nextN = &linkedList[i+1]
+		}
+		linkedList[i].Val = valuesFromLists[i]
+		linkedList[i].Next = nextN
+	}
+
+	return &linkedList[0]
 }
--- 9.merge-k-sorted-lists/main_test.go
+++ 9.merge-k-sorted-lists/main_test.go
@@ -10,25 +10,32 @@ func TestMergeKList(t *testing.T) {
 	testcases := []struct {
 		input  []*ListNode
 		answer *ListNode
-	}{}
+	}{
+		{input: []*ListNode{createListNodeFromSliceInt([]int{1, 4, 5}), createListNodeFromSliceInt([]int{1, 3, 4}), createListNodeFromSliceInt([]int{2, 6})}, answer: createListNodeFromSliceInt([]int{1, 1, 2, 3, 4, 4, 5, 6})},
+	}
 	for tcIdx, tc := range testcases {
 		t.Run(fmt.Sprintf("#%d", tcIdx), func(t *testing.T) {
 			actual := MergeKLists(tc.input)
 			actualVals := linkedListValuesToSliceInt(actual)
 			answerVals := linkedListValuesToSliceInt(tc.answer)
 			if !reflect.DeepEqual(actualVals, answerVals) {
-				t.Errorf("Got %v expected %v", actual, tc.answer)
+				t.Errorf("Got %v expected %v", actualVals, answerVals)
 			}
 		})
 	}
 }
 
-func sliceIntToLinkedList(s []int) *ListNode {
-	var linkedListNodes []*ListNode
+func createListNodeFromSliceInt(s []int) *ListNode {
+	linkedList := make([]ListNode, len(s))
 	for i := 0; i < len(s); i++ {
-		linkedListNodes = append(linkedListNodes, &ListNode{Val: s[i]})
+		var next *ListNode
+		if i != len(s)-1 {
+			next = &linkedList[i+1]
+		}
+		linkedList[i].Val = s[i]
+		linkedList[i].Next = next
 	}
-	return linkedListNodes[0]
+	return &linkedList[0]
 }
 func linkedListValuesToSliceInt(linkedList *ListNode) []int {
 	var vals []int
